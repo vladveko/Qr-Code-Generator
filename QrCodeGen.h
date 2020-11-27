5aa0ef60526293c0d6e02397e4cac01de071ff01
vlad_veko <veko.2001@gmail.com>
2020-11-27 23:46:08 +0300

    Added header file for QrCode and BitBuffer

--- QrCodeGen.h
+++ QrCodeGen.h
@@ -1,3 +1,37 @@
 #pragma once
 
 #include "resource.h"
+#include <vector>
+
+class QrCode {
+
+private:
+
+	void EncodeText(std::string);
+
+public:
+	QrCode();
+	~QrCode();
+
+	/* Основная функция, вызов которой генерирует QR-код */
+	void Generate();
+};
+
+
+class BitBuffer final : public std::vector<bool> {
+
+	/*---- Constructor ----*/
+
+	// Creates an empty bit buffer (length 0).
+public: BitBuffer();
+
+
+
+		/*---- Method ----*/
+
+		// Appends the given number of low-order bits of the given value
+		// to this buffer. Requires 0 <= len <= 31 and val < 2^len.
+public: 
+	void appendBits(std::uint32_t val, int len);
+
+};
